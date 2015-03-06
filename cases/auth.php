7051e29c81026f2e55fe8950ce6611de873613c8
fmrafael <rafaelfernandesmarcondes@gmail.com>
2015-03-05 23:42:12 -0300

    1

---
+++ cases/auth.php
@@ -0,0 +1,26 @@
+<?php 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once "vendor/autoload.php";
+$linkedIn=new Happyr\LinkedIn\LinkedIn('v3x1pj441nl9', '3ifR7tAuTKrzTUHH');
+if ($linkedIn->isAuthenticated()) {
+	//we know that the user is authenticated now. Start query the API
+	$user=$linkedIn->api('v1/people/~:(firstName,lastName,headline,location,num-connections,summary,specialties,positions,picture-url,public-profile-url,email-address,date-of-birth,educations,num-recommenders)');	
+	echo "Welcome ".$user['firstName'] .$user['lastName'] .$user['location']['name'] ;
+	
+	exit();
+	
+} elseif ($linkedIn->hasError()) {
+	echo "User canceled the login.";
+	exit();
+}
+//if not authenticated
+$url = $linkedIn->getLoginUrl(array(
+		redirect_uri => 'http://6e34f295.ngrok.com/cases/auth.php'));
+
+header("Location: " . $url);
+
+
+?>
---
+++ cases/config.php
@@ -0,0 +1,10 @@
+<?php 
+$mysql_hostname = "localhost";
+$mysql_user = "root";
+$mysql_password = "102030";
+$mysql_database = "mendl_challenge";
+$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
+mysql_select_db($mysql_database, $bd) or die("Could not select database");
+$base_url='http://6e34f295.ngrok.com/';
+
+?>
