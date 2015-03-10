72612e0d7ab25d65f2af821d9f950462bec66f8b
fmrafael <rafaelfernandesmarcondes@gmail.com>
2015-03-09 23:59:35 -0300

    db_ok

--- cases/auth.php
+++ cases/auth.php
@@ -1,24 +1,50 @@
 <?php 
 
+//mysql configuration
+$mysql_hostname = "localhost";
+$mysql_user = "root";
+$mysql_password = "102030";
+$mysql_database = "mendl_challenge";
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
 
 require_once "vendor/autoload.php";
 $linkedIn=new Happyr\LinkedIn\LinkedIn('v3x1pj441nl9', '3ifR7tAuTKrzTUHH');
 if ($linkedIn->isAuthenticated()) {
 	//we know that the user is authenticated now. Start query the API
-	$user=$linkedIn->api('v1/people/~:(firstName,lastName,headline,location,num-connections,summary,specialties,positions,picture-url,public-profile-url,email-address,date-of-birth,educations,num-recommenders)');	
-	echo "Welcome ".$user['firstName'] .$user['lastName'] .$user['location']['name'] ;
+	$user=$linkedIn->api('v1/people/~:(id,formattedName,numConnections,pictureUrl,publicProfileUrl,emailAddress)');	
+	echo "Welcome ".$user['id'] .$user['formattedName'] .$user['publicProfileUrl'];	
+	$conn = new mysqli($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
 	
-	exit();
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}	
+	// Get variables api and insert in database
 	
-} elseif ($linkedIn->hasError()) {
+	
+	$sql = "INSERT INTO users (id_linkedin,formattedName,numConnections,pictureUrl,publicProfileUrl,emailAddress) VALUES('$user[id]','$user[formattedName]','$user[numConnections]','$user[pictureUrl]','$user[publicProfileUrl]','$user[emailAddress]')";
+	
+		if (mysqli_query($conn, $sql)) {
+			echo "New record created successfully";
+		} else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+		exit();			
+	
+		
+	
+			
+}
+elseif ($linkedIn->hasError()) {
 	echo "User canceled the login.";
 	exit();
 }
 //if not authenticated
 $url = $linkedIn->getLoginUrl(array(
-		redirect_uri => 'http://6e34f295.ngrok.com/cases/auth.php'));
+		redirect_uri => 'http://3737234d.ngrok.com/cases/auth.php'));
 
 header("Location: " . $url);
 
