6ecab148db1d2a78ebf9140a8dcf065b52299e87
fmrafael <rafaelfernandesmarcondes@gmail.com>
2015-03-31 22:58:40 -0300

    v6

---
+++ cases/insert_db.php
@@ -0,0 +1,17 @@
+<?php 
+//mysql configuration
+$mysql_hostname = "localhost";
+$mysql_user = "root";
+$mysql_password = "102030";
+$mysql_database = "mendl_challenge";
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'config.php';		
+
+mysqli_query($conn,"INSERT INTO cases (companyName,caseTitle,caseDescription,processSelection) 
+		VALUES ('Case-teste','Como ampliar as vendas?', 
+		'Teste.', '#RED23456')") or die(mysqli_error($conn));
+
+?>
