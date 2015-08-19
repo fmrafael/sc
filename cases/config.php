57bab7546d8a3f540500476ead76ac3514002618
fmrafael <rafaelfernandesmarcondes@gmail.com>
2015-08-19 00:34:20 -0300

    limpo

--- cases/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-//mysql configuration
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "102030";
-$mysql_database = "mendl_challenge";
-$conn = new mysqli($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
-?>
--- cases/linkedin_lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Linkedin Application Settings
-$linkedin_appKey='v3x1pj441nl9';
-$linkedin_appSecret='3ifR7tAuTKrzTUHH';
-$linkedin_callbackUrl=$base_url.'linkedin_login.php';
-
-));
-?>
